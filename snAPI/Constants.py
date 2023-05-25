884eca932d679827001778c37a2ff1ea39f9bb84
Torsten Krause <krause@picoquant.com>
2023-05-25 11:05:08 +0200

    add python wrapper

---
+++ snAPI/Constants.py
@@ -0,0 +1,191 @@
+from enum import Enum, IntFlag, auto
+
+class DeviceType(Enum):
+    """
+The MultiHarp devices can use their internal crystal clock but alternatively they can be synchronized
+to an external reference clock. The latter is important when clock drift between multiple instruments in
+an experiment must be avoided. The constants defined here specify the reference clock to use.
+    """
+
+    Undefined = -1
+    """
+    This is for internal use only. 
+    
+    """
+    HW = 0
+    """
+    This is a real device, for instance a MultiHarp.
+    
+    """
+    File = 1
+    """
+    This is a file device. That means a ptu File that is opened as a device.
+    
+    """
+    
+class MeasMode(Enum):
+    """
+The PicoQuant TCSPC devices support different measurement modes. 
+The constants defined here specify the measurement mode the device gets initialized for with :meth:`snAPI.initDevice<snAPI.Main.snAPI.initDevice>`.
+
+In histogram mode, the photon arrival time data is instantly processed to calculate the time differences 
+between sync events and events on the input channels. These get then accumulated in a set of histograms,
+one for each input channel. This is typically used for classic fluorescence lifetime measurements with
+TCSPC and requires very little data processing effort by the user. As opposed to this, the time tagging 
+modes T2 and T3 are designed to stream the raw photon arrival time data to the host PC where the data
+can be processed and analyzed with ultimate flexibility for a huge range of more advanced applications.
+This processing can be carried out "on the fly" while the measurement is running, alternatively the data 
+can be streamed to a file or PC memory and be processed later. The snAPI library supports both approaches.
+
+
+    """
+
+    Histogram = 0
+    """
+**Histogram Mode**
+
+For the measurement class :class:`.Histogram` it is useful to initialize the device in Histogram mode.
+For devices that support hardware histogramming this will enable higher count rates than software processing
+of the raw stream of time tags.
+
+Note
+----
+    In histogram  mode no data is written to disc because the raw data stream is processed internally and
+    does not get transmitted to the API.
+
+    
+    """
+    T2 = 2
+    """
+**T2 mode**
+
+    .. image:: _static/T2_mode.jpg
+        :class: p-2
+    
+    In T2 mode all timing inputs and the sync input are functionally identical. There is no dedication
+    of the sync input channel to a sync signal from a laser. It may be left unconnected or can be used
+    for an additional detector signal. Usually all regular inputs are used to connect photon detectors.
+    The events from all channels are recorded independently and treated equally. In each case an event
+    record is generated that contains information about the channel it came from and the arrival time
+    of the event with respect to the overall measurement start. The timing is recorded with the highest
+    resolution the hardware supports. 
+    Dead-times exist only within each channel but not across the channels.
+    
+    """
+
+    T3 = 3
+    """
+**T3 Mode**
+
+    .. image:: _static/T3_mode.jpg
+        :class: p-2
+    
+    In T3 mode the sync input is dedicated to a periodic sync signal, typically from a laser. As far as
+    the experimental setup is concerned, this is similar to histogram mode. The main objective is to
+    allow high sync rates which could not be handled in T2 mode due to deadtime and USB bandwith limits. 
+    Accommodating the high sync rates in T3 mode is achieved as follows: 
+    First, a sync divider is employed (as in histogram mode). 
+    This reduces the sync rate so that the channel dead-time is no longer a problem. The remaining
+    problem is that even with the divider the sync rate may still be too high for collecting all
+    individual sync events like ordinary T2 mode events. Considering that sync events are not of primary
+    interest, the solution is to record them only if they arrive in the context of a photon event on any
+    of the input channels. The event record is then composed of two timing figures:
+
+    - the start stop timing difference between the photon event and the last sync event, and
+    - the arrival time of the event pair on the overall experiment time scale (the time tag).
+    
+    The latter is obtained by simply counting sync pulses. From the T3 mode event records it is therefore
+    possible to precisely determine which sync period a photon event belongs to. Since the sync period
+    is also known precisely, this furthermore allows to reconstruct the arrival time of the photon with
+    respect to the overall experiment time.
+    
+    """
+
+class MeasControl(Enum):
+    """this constant is for software or hardware starting and stopping (triggered) measurements in :meth:`Device.setMeasControl<snAPI.Main.Device.setMeasControl>`."""
+
+    SingleShotCTC = 0
+    """Acquisition starts by software command and runs until CTC expires. The duration is set by the
+    aqcTime parameter (>0)."""
+    
+    C1Gated = 1
+    """Data is collected for the period where C1 is active. This can be the logical high or low period
+    dependent on the value supplied to the parameter startEdge."""
+    
+    C1StartCtcStop = 2
+    """Data collection is started by a transition on C1 and stopped by expiration of the internal CTC.
+    Which transition actually triggers the start is given by the value supplied to the parameter startEdge.
+    The duration is set by the aqcTime parameter (>0)."""
+    
+    C1StartC2Stop = 3
+    """Data collection is started by a transition on C1 and stopped by by a transition on C2. Which transitions
+    actually trigger start and stop is given by the values supplied to the parameters startEdge and stopEdge."""
+    
+    WrM2S = 4
+    """White Rabbit master to slave (for further use)"""
+    
+    WrS2M = 5
+    """White Rabbit slave to master (for further use)"""
+    
+    SwStartSwStop = 6
+    """Software Start and Software Stop is in use if aqcTime parameter is 0."""
+    
+    
+class RefSource(Enum):
+    """
+The MultiHarp devices can use their internal crystal clock but alternatively they can be synchronized
+to an external reference clock. The latter is important when clock drift between multiple instruments in
+an experiment must be avoided. The constants defined here specify the reference clock to use."""
+
+    Internal = 0
+    """
+| **Internal clock**
+| This is the default and normally what most users would select when only one device is being used. 
+
+    """
+    External_10MHZ = 1
+    """
+    **10MHz external clock**
+    This is for use with an industry standard 10MHz reference such as an atomic clock or another MultiHarp.
+    
+    """
+    Wr_Master_Generic = 2
+    """
+    **White Rabbit master with generic partner**
+    
+    """
+    Wr_Slave_Generic = 3
+    """
+    **White Rabbit slave with generic partner**
+    
+    """
+    Wr_Grandm_Generic = 4
+    """
+    **White Rabbit grand master with generic partner**
+    
+    """
+    Extn_GPS_PPS = 5
+    """
+    **10 MHz + PPS from GPS**
+    
+    """
+    Extn_GPS_PPS_UART = 6
+    """
+    **10 MHz + PPS + time via UART from GPS**
+    
+    """
+    Wr_Master_Mharp = 7
+    """
+    **White Rabbit master with MultiHarp as partner**
+    
+    """
+    Wr_Salve_Mharp = 8
+    """
+    **White Rabbit slave with MultiHarp as partner**
+    
+    """
+    Wr_Grandm_Mharp = 9
+    """
+    **White Rabbit grand master with MultiHarp as partner**
+    
+    """
---
+++ snAPI/Utils.py
@@ -0,0 +1,128 @@
+class Color:
+    Rst ='\033[0m'
+
+        # Regular
+    Bla ='\033[0;30m'
+    """regular black"""
+    Red ='\033[0;31m'
+    """regular red"""
+    Gre ='\033[0;32m'
+    """regular green"""
+    Yel ='\033[0;33m'
+    """regular yellow"""
+    Blu ='\033[0;34m'
+    """regular blue"""
+    Pur ='\033[0;35m'
+    """regular purple"""
+    Cya ='\033[0;36m'
+    """regular cyan"""
+    Whi ='\033[0;37m'
+    """regular white"""
+
+        # Bold
+    BBla ='\033[1;30m'
+    """bold black"""
+    BRed ='\033[1;31m'
+    """bold red"""
+    BGre ='\033[1;32m'
+    """bold green"""
+    BYel ='\033[1;33m'
+    """bold yellow"""
+    BBlu ='\033[1;34m'
+    """bold blue"""
+    BPur ='\033[1;35m'
+    """bold purple"""
+    BCya ='\033[1;36m'
+    """bold cyan"""
+    BWhi ='\033[1;37m'
+    """bold white"""
+
+        # Underline
+    UBla ='\033[4;30m'
+    """underline black"""
+    URed ='\033[4;31m'
+    """underline red"""
+    UGre ='\033[4;32m'
+    """underline green"""
+    UYel ='\033[4;33m'
+    """underline yellow"""
+    UBlu ='\033[4;34m'
+    """underline blue"""
+    UPur ='\033[4;35m'
+    """underline purple"""
+    UCya ='\033[4;36m'
+    """underline cyan"""
+    UWhi ='\033[4;37m'
+    """underline white"""
+
+        # High Intensity
+    IBla ='\033[0;90m'
+    """high intensity black"""
+    IRed ='\033[0;91m'
+    """high intensity red"""
+    IGre ='\033[0;92m'
+    """high intensity green"""
+    IYel ='\033[0;93m'
+    """high intensity yellow"""
+    IBlu ='\033[0;94m'
+    """high intensity blue"""
+    IPur ='\033[0;95m'
+    """high intensity purple"""
+    ICya ='\033[0;96m'
+    """high intensity cyan"""
+    IWhi ='\033[0;97m'
+    """high intensity white"""
+
+        # BoldHigh Intens
+    BIBla ='\033[1;90m'
+    """bold high intensity black"""
+    BIRed ='\033[1;91m'
+    """bold high intensity red"""
+    BIGre ='\033[1;92m'
+    """bold high intensity green"""
+    BIYel ='\033[1;93m'
+    """bold high intensity yellow"""
+    BIBlu ='\033[1;94m'
+    """bold high intensity blue"""
+    BIPur ='\033[1;95m'
+    """bold high intensity purple"""
+    BICya ='\033[1;96m'
+    """bold high intensity cyan"""
+    BIWhi ='\033[1;97m'
+    """bold high intensity white"""
+
+        # Background
+    On_Bla ='\033[40m'
+    """background black"""
+    On_Red ='\033[41m'
+    """background red"""
+    On_Gre ='\033[42m'
+    """background green"""
+    On_Yel ='\033[43m'
+    """background yellow"""
+    On_Blu ='\033[44m'
+    """background blue"""
+    On_Pur ='\033[45m'
+    """background purple"""
+    On_Cya ='\033[46m'
+    """background cyan"""
+    On_Whi ='\033[47m'
+    """background white"""
+
+        # High Intensity Backgrounds
+    On_IBla ='\033[0;100m'
+    """high intensity background black"""
+    On_IRed ='\033[0;101m'
+    """high intensity background red"""
+    On_IGre ='\033[0;102m'
+    """high intensity background green"""
+    On_IYel ='\033[0;103m'
+    """high intensity background yellow"""
+    On_IBlu ='\033[0;104m'
+    """high intensity background blue"""
+    On_IPur ='\033[0;105m'
+    """high intensity background purple"""
+    On_ICya ='\033[0;106m'
+    """high intensity background cyan"""
+    On_IWhi ='\033[0;107m'
+    """high intensity background white"""
